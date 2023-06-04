746a162a89674e89f465416fd1e0d8cc1acef915
sonpipe0 <ffernandezcronenbol@mail.austral.edu.ar>
2023-06-04 01:31:10 -0300

    ant to gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "algebra_3"
+
---
+++ src/main/java/org/algebra_3/Main.java
@@ -0,0 +1,7 @@
+package org.algebra_3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
